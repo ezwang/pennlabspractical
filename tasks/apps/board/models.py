360046fcfbc46a0a80d7224657d2d300948066d4
Eric Wang <ezwang1@gmail.com>
2017-09-16 15:16:04 -0400

    fix models

--- tasks/apps/board/models.py
+++ tasks/apps/board/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 
-class Card(models.Model):
-    title = models.TextField()
-    order = models.IntegerField()
-
-
 class List(models.Model):
     title = models.TextField()
+    order = models.IntegerField(unique=True)
+
+
+class Card(models.Model):
+    title = models.TextField()
     description = models.TextField(blank=True)
-    listId = models.ForeignKey("List", on_delete=models.CASCADE)
+    listId = models.ForeignKey(List, null=True, on_delete=models.CASCADE)
