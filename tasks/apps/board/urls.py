86bb9a9eb38fb2e21ffda0764b719a943f5b5055
Eric Wang <ezwang1@gmail.com>
2017-09-16 16:21:20 -0400

    add empty frontend page

--- tasks/apps/board/urls.py
+++ tasks/apps/board/urls.py
@@ -3,9 +3,14 @@ from django.conf.urls import url
 from .views import frontend, api
 
 urlpatterns = [
+    # Frontend
+    url(r"^$", frontend.index_view, name="index"),
+
+    # Card API Endpoints
     url(r"^card$", api.add_card_view, name="add_card"),
     url(r"^card/(?P<cardId>\d+)", api.view_delete_card_view, name="view_card"),
 
+    # List API Endpoints
     url(r"^list$", api.add_list_view, name="add_list"),
     url(r"^list/(?P<listId>\d+)$", api.view_delete_list_view, name="view_list"),
     url(r"^editlist/(?P<listId>\d+)$", api.edit_list_view, name="edit_list"),
