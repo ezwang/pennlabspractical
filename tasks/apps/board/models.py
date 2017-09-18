8282ade91b847cc10d3ca245f72916a43360b558
Eric Wang <ezwang1@gmail.com>
2017-09-17 23:41:49 -0400

    document models

--- tasks/apps/board/models.py
+++ tasks/apps/board/models.py
@@ -2,11 +2,27 @@ from django.db import models
 
 
 class List(models.Model):
+    """
+    Represents a list object.
+
+    Attributes:
+        title - The title of the list.
+        order - The order of the list, ranging from 1 to the total number of lists (inclusive).
+    """
+
     title = models.TextField()
     order = models.IntegerField()
 
 
 class Card(models.Model):
+    """
+    Represents a card object.
+
+    Attributes:
+        title - The title of the card.
+        description - The card description (optional, can be blank).
+        ls - The list that this card is attached to. Use ls.id for the list ID.
+    """
     title = models.TextField()
     description = models.TextField(blank=True)
     ls = models.ForeignKey(List, null=True, on_delete=models.CASCADE)
